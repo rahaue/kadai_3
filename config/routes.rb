cc353b0d01759e852eab0b6eca0b06c49ea0ca7a
EC2 Default User <ec2-user@ip-172-31-34-38.ap-northeast-1.compute.internal>
2020-10-24 08:35:28 +0000

    firts-commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
     
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'homes#top'
-  
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  post 'books' => 'books#create'
-  delete 'books/:id' => 'books#destroy'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
+  root to: 'homes#top'
+  resources :books
   
 end
